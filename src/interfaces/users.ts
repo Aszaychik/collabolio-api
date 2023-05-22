6da166c0c6563f49aee719785ffe145472c1d896
AsZaychik <aszaychik@gmail.com>
2023-05-22 22:26:00 +0700

    new interface schema for mongodb

---
+++ src/interfaces/users.ts
@@ -0,0 +1,52 @@
+export interface Users {
+  _id: ObjectId;
+  uid: string;
+  username: string;
+  googleId?: string;
+  email: string;
+  emailVerified: boolean;
+  password: string;
+  isAdmin: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+  lastLoginAt?: Date;
+  profile: {
+    displayName: string;
+    phoneNumber?: string;
+    bio: string;
+    photoURL: string;
+    location?: string;
+    skills?: { name: string }[];
+    interests?: { name: string }[];
+    experience?: {
+      title: string;
+      company: string;
+      startDate: Date;
+      endDate: Date;
+      description: string;
+    }[];
+    education?: {
+      degree: string;
+      school: string;
+      startDate: Date;
+      endDate: Date;
+      grade?: number;
+    }[];
+    projects?: {
+      title: string;
+      description: string;
+      link: string;
+    }[];
+    certifications?: {
+      name: string;
+      credentialURL: string;
+      endDate?: Date;
+      skills?: { name: string }[];
+    }[];
+    languages?: { name: string }[];
+    socialLinks?: {
+      name: string;
+      url: string;
+    }[];
+  };
+}
