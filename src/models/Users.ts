57ea9273212ee952f7fa5170f068b45b80860bb1
AsZaychik <aszaychik@gmail.com>
2023-05-23 00:47:27 +0700

    User Models

---
+++ src/models/Users.ts
@@ -0,0 +1,113 @@
+import mongoose, { Schema } from 'mongoose';
+import { IUsers } from '../interfaces/usersInterface';
+
+const userSchema: Schema = new mongoose.Schema({
+  username: String,
+  googleId: {
+    type: String,
+    required: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  emailVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  lastLoginAt: {
+    type: Date,
+    required: false,
+  },
+  profile: {
+    type: {
+      displayName: String,
+      phoneNumber: String,
+      bio: String,
+      photoURL: String,
+      location: String,
+      skills: [
+        {
+          name: String,
+        },
+      ],
+      interests: [
+        {
+          name: String,
+        },
+      ],
+      experience: [
+        {
+          title: String,
+          company: String,
+          startDate: Date,
+          endDate: Date,
+          description: String,
+        },
+      ],
+      education: [
+        {
+          degree: String,
+          school: String,
+          startDate: Date,
+          endDate: Date,
+          grade: Number,
+        },
+      ],
+      projects: [
+        {
+          title: String,
+          description: String,
+          link: String,
+        },
+      ],
+      certifications: [
+        {
+          name: String,
+          credentialURL: String,
+          endDate: Date,
+          skills: [
+            {
+              name: String,
+            },
+          ],
+        },
+      ],
+      languages: [
+        {
+          name: String,
+        },
+      ],
+      socialLinks: [
+        {
+          name: String,
+          url: String,
+        },
+      ],
+    },
+    required: true,
+  },
+});
+
+export default mongoose.model<IUsers>('Users', userSchema);
