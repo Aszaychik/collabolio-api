e875f347eb67b44ff2cf5f818d0db7399b81dd7e
AsZaychik <aszaychik@gmail.com>
2023-05-22 21:46:01 +0700

    clearing all firebase

--- src/services/firebase.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as serviceAccountKey from './serviceAccountKey.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount),
-  databaseURL: 'https://collabolio-dev.firebaseio.com',
-});
-
-export default admin;
