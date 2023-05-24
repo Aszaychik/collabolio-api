16786e437404647c466728aa8568c484667a2876
AsZaychik <aszaychik@gmail.com>
2023-05-24 14:30:59 +0700

    delete hello worlds

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import { helloWorld } from '../controllers/helloControllers';
 import authRoutes from './authRoutes';
 // Create router
 const router = Router();
 
 // Routes
-router.get('/', helloWorld);
 router.use('/auth', authRoutes);
 
 export default router;
