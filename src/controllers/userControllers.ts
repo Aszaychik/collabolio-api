ff6677b5175d596c22fe562730358fef43f740e3
AsZaychik <aszaychik@gmail.com>
2023-05-24 19:52:40 +0700

    not using create,delete,all user

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -24,51 +24,6 @@ export const getUser = async (req: Request, res: Response) => {
   }
 };
 
-export const getUsers = async (req: Request, res: Response) => {
-  try {
-    const users = await Users.find();
-    res
-      .status(200)
-      .json({ message: 'Users found successfully', users, statusCode: 200 });
-  } catch (error) {
-    res.status(500).json({ message: error.message, statusCode: 500 });
-  }
-};
-
-export const createUser = async (req: Request, res: Response) => {
-  try {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const user = await Users.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    res
-      .status(200)
-      .json({ message: 'User created successfully', user, statusCode: 200 });
-  } catch (error) {
-    res.status(500).json({ message: error.message, statusCode: 500 });
-  }
-};
-
-export const deleteUser = async (req: Request, res: Response) => {
-  try {
-    const { _id } = req.params;
-    const user = await Users.findByIdAndDelete(_id);
-    if (user) {
-      res
-        .status(200)
-        .json({ message: 'User deleted successfully', user, statusCode: 200 });
-    } else {
-      res.status(404).json({ message: 'User not found', statusCode: 404 });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message, statusCode: 500 });
-  }
-};
-
 export const updateUser = async (req: Request, res: Response) => {
   try {
     const { _id } = req.body.currentUser;
@@ -90,3 +45,48 @@ export const updateUser = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message, statusCode: 500 });
   }
 };
+
+// export const getUsers = async (req: Request, res: Response) => {
+//   try {
+//     const users = await Users.find();
+//     res
+//       .status(200)
+//       .json({ message: 'Users found successfully', users, statusCode: 200 });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message, statusCode: 500 });
+//   }
+// };
+
+// export const createUser = async (req: Request, res: Response) => {
+//   try {
+//     const { username, email, password } = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10);
+
+//     const user = await Users.create({
+//       username,
+//       email,
+//       password: hashedPassword,
+//     });
+//     res
+//       .status(200)
+//       .json({ message: 'User created successfully', user, statusCode: 200 });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message, statusCode: 500 });
+//   }
+// };
+
+// export const deleteUser = async (req: Request, res: Response) => {
+//   try {
+//     const { _id } = req.params;
+//     const user = await Users.findByIdAndDelete(_id);
+//     if (user) {
+//       res
+//         .status(200)
+//         .json({ message: 'User deleted successfully', user, statusCode: 200 });
+//     } else {
+//       res.status(404).json({ message: 'User not found', statusCode: 404 });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ message: error.message, statusCode: 500 });
+//   }
+// };
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,21 @@
 import { Router } from 'express';
 import {
   getUser,
-  getUsers,
-  createUser,
   updateUser,
-  deleteUser,
+  //getUsers,
+  //createUser,
+  //deleteUser,
 } from '../controllers/userControllers';
+
+import { authMiddleware } from '../middleware/auth';
 
 const router = Router();
 
-router.get('/:id', getUser);
-router.get('/', getUsers);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+// router.get('/', getUsers);
+// router.post('/', createUser);
+// router.delete('/:id', deleteUser);
+router.use(authMiddleware);
+router.get('/', getUser);
+router.put('/', updateUser);
 
 export default router;
