1f6204940068854b07c2f2f01fb5ef95aaa9bdad
AsZaychik <aszaychik@gmail.com>
2023-05-24 16:17:20 +0700

    welcoming page use static file

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config(); // Configure the environment variables
 
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import routes from './routes/routes';
 import cors from 'cors';
 // import { authMiddleware } from './middleware/auth.js';
 import db from './db/mongoDb';
+import path from 'path';
 
 db;
 
@@ -14,6 +15,11 @@ export const app: Application = express(); // Create the Express application
 
 app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS) middleware to allow requests from different origins
 app.use(express.json()); // Parse incoming requests with JSON payloads
+
+// Serve the index.html file as the default page
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // app.use(authMiddleware); // Add the authorization middleware (for checking the JWT token in the request header)
 app.use('/api', routes); // mount the routes
