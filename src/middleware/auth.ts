8cc74e023bceee02a9898831023672400caf7338
AsZaychik <aszaychik@gmail.com>
2023-05-24 20:45:05 +0700

    middleware done

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,30 +1,37 @@
-import jwt, { JwtPayload } from 'jsonwebtoken';
+import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import Users from '../models/Users';
 
-export const authMiddleware = (
+export const authMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ message: 'Authorization header missing' });
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Bearer token missing or invalid' });
   }
-
   const token: string = authHeader.split(' ')[1];
 
   try {
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string);
-    const { _id } = decodedToken as JwtPayload;
-
-    const user = Users.findById(_id);
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid or expired token' });
-    }
-    req.body.currentUser = user;
-    next();
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET,
+      async (err: VerifyErrors, payload: JwtPayload) => {
+        if (err) {
+          return res.status(401).json({ err });
+        }
+        console.log(payload);
+        const { userId } = payload;
+        const user = await Users.findById(userId);
+        console.log(userId);
+        if (!user) {
+          return res.status(401).json({ message: 'User not found' });
+        }
+        req.body.currentUser = user;
+        next();
+      },
+    );
   } catch (error) {
     console.error(error);
     return res.status(401).json({ message: error.message });
