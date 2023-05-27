5263693dc7b739afab1941bec8c83947bfc83848
AsZaychik <aszaychik@gmail.com>
2023-05-28 02:05:59 +0700

    lmao changing avatar upload to photo controllers

---
+++ src/controllers/profileControllers.ts
@@ -0,0 +1,60 @@
+import { Response } from 'express';
+import { IReqUplodFile } from '../interfaces/IUploadFile';
+import storage from '../services/cloudStorage';
+import Users from '../models/Users';
+
+export const uploadProfilePhoto = (req: IReqUplodFile, res: Response) => {
+  if (!req.file) {
+    return res.status(400).json({
+      message: 'No file uploaded',
+      success: false,
+    });
+  }
+
+  const bucket = storage.bucket(`gs://${process.env.BUCKET_NAME}`);
+
+  const blob = bucket.file(
+    `users/${req.currentUser._id}/photo/${req.file.originalname}`,
+  );
+
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+    metadata: {
+      contentType: req.file.mimetype,
+    },
+  });
+
+  blobStream.on('error', (error) => {
+    return res.status(400).json({
+      message: error.message,
+      success: false,
+    });
+  });
+
+  blobStream.on('finish', async () => {
+    req.file.url = `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${blob.name}`;
+
+    const { _id } = req.currentUser;
+    try {
+      const user = await Users.findByIdAndUpdate(
+        _id,
+        {
+          'profile.photoURL': req.file.url,
+          updatedAt: new Date(),
+        },
+        { new: true },
+      );
+      return res.status(200).json({
+        message: 'Photo uploaded successfully',
+        success: true,
+        data: req.file.url,
+        user: user,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message,
+        success: false,
+      });
+    }
+  });
+};
